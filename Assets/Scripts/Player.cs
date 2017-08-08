e4c914ec2162efe2c21879d65d1dac0b86a6d5b3
BrettKingMoses <brettking96@outlook.com>
2017-08-08 15:50:01 +1000

    // // //

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,5 +24,9 @@ public class Player : MonoBehaviour
         Vector3 velocity = new Vector3(0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0);
         pos += rot * velocity;
         transform.position = pos;
+        //
+        //
+        //
+
     }
 }
